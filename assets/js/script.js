842d2f5968521e0eedb36bb368c630521cf524e8
matthewbrownlow <>
2023-06-13 19:37:57 +0000

    Add function to generate computer's choice + comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
-// Function to generate computer's choice
+/**
+ * When this function is called, it randomly selects and returns one of the three choices
+ * ("rock", "paper", or "scissors") as the computer's play in the game
+ */
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+/** 
+ * Function to determine the winner of the game
+ * */
 
 
-// Function to determine the winner of the game
+
+/** 
+ * Function to update the result message
+ */
 
 
-// Function to update the result message
+
+/** 
+ * Function to start the game
+ */
 
 
-// Function to start the game
 
-
-// Start the game
+/**
+ * Start the game
+ */
