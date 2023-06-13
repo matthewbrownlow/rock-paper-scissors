8346ffa9aea4c1daf1ee6fe997cf58f298bba85d
matthewbrownlow <>
2023-06-13 19:22:35 +0000

    Add font awesome link and icons, Add JavaScript comment placeholders

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Function to generate computer's choice
+
+
+// Function to determine the winner of the game
+
+
+// Function to update the result message
+
+
+// Function to start the game
+
+
+// Start the game
